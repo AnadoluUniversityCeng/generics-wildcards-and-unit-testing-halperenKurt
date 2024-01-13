d0390b6c6e2986b954124c856ac463a48d6e90ac
Hasan Alperen KURT <kurtalperen96@gmail.com>
2024-01-13 12:14:35 +0300

    fixed

--- src/test/java/edu/estu/FailingTest.java
+++ src/test/java/edu/estu/FailingTest.java
@@ -1,27 +1,20 @@
 package edu.estu;
 
 import org.junit.Test;
-import static org.junit.Assert.fail;
 
-/* *********************************************************
- **** WRITE YOUR FAILING UNIT TEST(S) INTO  THIS FILE! *****
- ***********************************************************
- */
+import static org.junit.Assert.assertTrue;
 
-/**
- * Unit tests for the first part of the project.
- */
+
 public class FailingTest {
-    @Test
-    public void testInfiniteLoopBug() {
-        try {
-            // Provide an input that triggers the infinite loop
-            App.infiniteLoop(Double.POSITIVE_INFINITY);
 
-            // If the above line does not throw an exception, fail the test
-            fail("Expected infinite loop, but the method completed successfully.");
-        } catch (Exception expected) {
-            // The test passes if an exception is thrown (intentional bug)
+    @Test(timeout = 1000)
+    public void testInfiniteLoop() {
+        try {
+            App.infiniteLoop(Double.POSITIVE_INFINITY);
+            assertTrue(false); 
+        } catch (Throwable e) {
+            assertTrue(true); 
         }
     }
+
 }
